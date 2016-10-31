05e2c976ce7535d83dafb4ab0c8af6871f451fe3
Raúl Hernández <rhernandez@idealista.com>
2016-10-31 15:23:58 +0100

    Format corrected

--- src/whoIsNext.js
+++ src/whoIsNext.js
@@ -2,23 +2,22 @@
 //se ponen en cola y cada vez que uno bebe una bebida de la máquina, se clona y los dos clones se ponen a la cola al final
 //http://codeforces.com/problemset/problem/82/A
 
-var whoIsNext = function (placeInQueue) {
-  var names = ["Sheldon","Leonard","Penny","Rajesh","Howard"];
+var whoIsNext = function(placeInQueue) {
+  var names = [ "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" ];
   var queue = names;
   var position = placeInQueue - 1;
 
   if (placeInQueue <= 5) {
-    console.log("En el número " + placeInQueue + " está " + names[position] + " que no ha bebido aún" );
-    return(names[position]);
-  }
-  else {
-
-    for(var i=0; queue.length < placeInQueue; i ++){
+    console.log("En el número " + placeInQueue + " está " + names[position]
+        + " que no ha bebido aún");
+    return (names[position]);
+  } else {
+    for (var i = 0; queue.length < placeInQueue; i++) {
       queue.push(queue[i]);
       queue.push(queue[i]);
     }
-    console.log("En el número " + placeInQueue + " está: " + queue[i-1]);
-    return(queue[i-1]);
+    console.log("En el número " + placeInQueue + " está: " + queue[i - 1]);
+    return (queue[i - 1]);
   }
 };
 
