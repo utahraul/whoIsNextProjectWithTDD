37e00f93b03eafcf5ddb57a48bf8cf04ffdcff4c
utahraul <raul.hernandez@testeandosoftware.com>
2016-12-02 19:23:55 +0100

    Modified and translated the text according to the problem specified

--- src/whoIsNext.js
+++ src/whoIsNext.js
@@ -1,5 +1,8 @@
-//Sheldon, Leonard, Penny, Rajesh y Howard llegan a una máquina de bebidas,
-//se ponen en cola y cada vez que uno bebe una bebida de la máquina, se clona y los dos clones se ponen a la cola al final
+//One possible solution for http://codeforces.com/problemset/problem/82/A using Javascript and TDD.
+//Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on. This process continues ad infinitum.
+//For example, Penny drinks the third can of cola and the queue will look like this: Rajesh, Howard, Sheldon, Sheldon, Leonard, Leonard, Penny, Penny.
+//Write a program that will print the name of a man who will drink the n-th can.
+//Note that in the very beginning the queue looks like that: Sheldon, Leonard, Penny, Rajesh, Howard. The first person is Sheldon.
 //http://codeforces.com/problemset/problem/82/A
 
 var whoIsNext = function(placeInQueue) {
@@ -8,15 +11,15 @@ var whoIsNext = function(placeInQueue) {
   var position = placeInQueue - 1;
 
   if (placeInQueue <= 5) {
-    console.log("En el número " + placeInQueue + " está " + names[position]
-        + " que no ha bebido aún");
+    console.log("The name of the one who has drunk the drink number " + placeInQueue + " is " + names[position]
+        + " .");
     return (names[position]);
   } else {
     for (var i = 0; queue.length < placeInQueue; i++) {
       queue.push(queue[i]);
       queue.push(queue[i]);
     }
-    console.log("En el número " + placeInQueue + " está: " + queue[i - 1]);
+    console.log("The name of the one who has drunk the drink number " + placeInQueue + " is: " + queue[i - 1]);
     return (queue[i - 1]);
   }
 };
